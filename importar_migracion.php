25ade17e6bb11246d777ec532e19bbf4efe134e3
hweg000 <rubio.leonel1@gmail.com>
2025-12-04 09:10:08 -0600

    cambio de nombre en tabla

--- importar_migracion.php
+++ importar_migracion.php
@@ -39,7 +39,7 @@ $archivos = [
     ],
     [
         'nombre' => 'Datos Faltantes',
-        'archivo' => __DIR__ . '/migrations/gpoascen_pedidos_appdatos faltantes.sql',
+        'archivo' => __DIR__ . '/migrations/gpoascen_pedidos_appdatos_faltantes.sql',
         'descripcion' => 'Importa datos faltantes de tablas auxiliares'
     ],
     [
