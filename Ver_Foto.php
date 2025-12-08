7599160582cb0b751cf3409953b3ef0f5b307f39
hweg000 <rubio.leonel1@gmail.com>
2025-12-08 16:53:52 -0600

    Update Ver_Foto.php

--- Ver_Foto.php
+++ Ver_Foto.php
@@ -15,7 +15,7 @@
         // Función para hacer una solicitud HTTP GET
         function obtenerImagen() {
             var id_pedido = 91; // Establece el ID del pedido que deseas mostrar
-            var url = "107.21.163.64/Pedidos_GA/Pedidos_GA/App/Ver_Foto.php?id_pedido=" + id_pedido; // Reemplaza la URL con la ruta correcta a tu archivo PHP
+            var url = "https://pedidos.grupoascencio.com.mx/Pedidos_GA/App/Ver_Foto.php?id_pedido=" + id_pedido; // Reemplaza la URL con la ruta correcta a tu archivo PHP
 
             // Realizar la solicitud HTTP GET
             fetch(url)
