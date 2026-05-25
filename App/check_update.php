644eb2c614930202e39d026d60e82e48667278ba
ACasillas1999 <acasillas.work@gmail.com>
2026-05-25 12:05:25 -0600

    New APK

--- App/check_update.php
+++ App/check_update.php
@@ -6,12 +6,12 @@ header('Content-Type: application/json; charset=UTF-8');
 // require_once __DIR__ . '/Conexiones/Conexion.php';
 
 // Ajusta estos valores cada vez que publiques una nueva APK.
-$latestVersionCode = 1;        // Debe ser mayor que BuildConfig.VERSION_CODE actual
-$latestVersionName = '1.0';    // Texto que verá el usuario
+$latestVersionCode = 2;        // Debe ser mayor que BuildConfig.VERSION_CODE actual
+$latestVersionName = '2.0';    // Texto que verá el usuario
 
 // URL pública donde subes el APK de la nueva versión.
 // EJEMPLO: https://pedidos.grupoascencio.com.mx/apk/app_pedidos_v1.apk
-$apkUrl = 'http://107.21.163.64/Pedidos_GA/Pedidos_GA/App/Apks/app_pedidos_v4.apk';
+$apkUrl = 'http://107.21.163.64/Pedidos_GA/Pedidos_GA/App/Apks/Pedidos_GA_V5_WEB.apk';
 
 $response = [
     'ok' => true,
